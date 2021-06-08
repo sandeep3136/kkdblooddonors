b0d33a30f0a24cc67373b3f30695afedbad2ae4a
sandeep angara <sandeep333jab@gmail.com>
2021-06-08 17:54:46 +0530

    final changes

--- forminput/urls.py
+++ forminput/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('forminp/', include("forminp.urls")),
+    path('', include("forminp.urls")),
 ]
